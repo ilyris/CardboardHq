62d015149a389610b9fedc7bb8e6982b63181b82
Dylan Sieren <dylan.sieren01@gmail.com>
2024-08-04 22:47:57 -0600

    update route handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth";
 import { config } from "@/helpers/auth";
-import { NextApiRequest, NextApiResponse } from "next";
 
-// Named export for the GET method
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, config);
-}
-
-// Named export for the POST method
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, config);
-}
-
-// You can also export handlers for other HTTP methods (PUT, DELETE, etc.) if needed.
+const handler = NextAuth(config);
+export { handler as GET, handler as POST };
