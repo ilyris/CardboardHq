c59c2eb0adfdc2039bb3ee03fbead9fcb44870e3
Dylan Sieren <dylan.sieren01@gmail.com>
2024-08-12 09:00:10 -0600

    update cookieAuthHeader to use the 'Secure' cookie for testing

--- helpers/api/cookieAuthHeaders.ts
+++ helpers/api/cookieAuthHeaders.ts
@@ -2,14 +2,21 @@ import { cookies } from "next/headers";
 
 export const createAuthHeaders = (): HeadersInit => {
   const cookieStore = cookies();
-  const sessionToken = cookieStore.get("next-auth.session-token")?.value;
+
+  // Attempt to get the session token based on different possible cookie names
+  const sessionToken =
+    cookieStore.get("__Secure-next-auth.session-token")?.value || // Production cookie
+    cookieStore.get("next-auth.session-token")?.value; // Development cookie
 
   const headers: HeadersInit = {
     "Content-Type": "application/json",
   };
 
   if (sessionToken) {
-    headers["cookie"] = `next-auth.session-token=${sessionToken}`;
+    const cookieName = sessionToken.startsWith("__Secure-")
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
+    headers["cookie"] = `${cookieName}=${sessionToken}`;
   }
 
   return headers;
