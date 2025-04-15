3d7d88938943221e5417f941166d0f89c99e536e
Dylan Sieren <dylan.sieren01@gmail.com>
2025-04-15 09:59:34 -0600

    set redis timeout to 10 seconds to test if bumping up the connection can help

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -3,6 +3,7 @@ import Redis from "ioredis";
 
 const redis = new Redis(process.env.REDIS_URL!, {
   tls: process.env.REDIS_URL?.startsWith("rediss://") ? {} : undefined,
+  connectTimeout: 10000,
 });
 
 export default redis;
