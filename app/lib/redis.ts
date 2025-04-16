1be0381b9faafd5730f99956661a5cd8281863d4
Dylan Sieren <dylan.sieren01@gmail.com>
2025-04-16 14:20:43 -0600

    quick redis changes to test in prod and dev

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -1,8 +1,13 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL!, {
-  tls: process.env.REDIS_URL?.startsWith("rediss://") ? {} : undefined,
+const redis = new Redis(process.env.REDIS_TLS_URL ?? process.env.REDIS_URL!, {
+  tls: (process.env.REDIS_TLS_URL ?? process.env.REDIS_URL)?.startsWith(
+    "rediss://"
+  )
+    ? {}
+    : undefined,
   connectTimeout: 10000,
+  maxRetriesPerRequest: 1,
 });
 
 export default redis;
